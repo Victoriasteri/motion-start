e67cee9ba0314f34e39a05b15368385aad44323e
Victoria Bezazieva <victoria.bezazi@ngft.com>
2025-10-20 15:56:44 +0300

    replaced webm video with mp4

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -65,7 +65,7 @@ const HomePage: React.FC = () => {
             zIndex: 0,
           }}
         >
-          <source src="/bg_video.webm" type="video/webm" />
+          <source src="/bg_video.mp4" type="video/webm" />
           Your browser does not support the video tag.
         </video>
 
